86d272e73bbd72295878b1c1ed085998b0e64a62
Glitch (pwned-password) <none>
2018-02-24 18:35:45 +0000

    🍅📠 Checkpoint public/client.js:242011/32 views/index.html:242011/1

--- public/client.js
+++ public/client.js
@@ -5,14 +5,6 @@
 // add other scripts at the bottom of index.html
 
 $(function() {
-  console.log('hello world :o');
-  
-  $.get('/dreams', function(dreams) {
-    dreams.forEach(function(dream) {
-      $('<li></li>').text(dream).appendTo('ul#dreams');
-    });
-  });
-
   $('form').submit(function(event) {
     event.preventDefault();
     var dream = $('input').val();
