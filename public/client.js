6d2bd85e50f92226af4c65f9fd4dc7242ae138f3
Glitch (pwned-password) <none>
2018-02-24 20:30:04 +0000

    🏤🐊 Checkpoint public/client.js:242011/174

--- public/client.js
+++ public/client.js
@@ -2,9 +2,15 @@ $(function() {
   $('form').submit(function(event) {
     event.preventDefault();
     var pwd = $('input').val();
-    $.post('/checkpwd?' + $.param({pwd: pwd}), function() {
+      var hash = crypto.subtle.digest('SHA-1', new ArrayBuffer(pwd));
       
-    });
+      hash.then(function(result) {
+        alert(result);
+      }, function(err) {
+        alert(err);
+      });
+    // $.post('/checkpwd?' + $.param({pwd: pwd}), function() {
+    // });
   });
 
 });
