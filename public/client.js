be4d4f07c87f8cf23433380885130585866b9ec8
Glitch (pwned-password) <none>
2018-02-24 20:50:11 +0000

    working crypto

--- public/client.js
+++ public/client.js
@@ -3,11 +3,14 @@ $(function() {
     event.preventDefault();
     var pwd = $('input').val();
     var enc = new TextEncoder('utf-8');
-    var prom = crypto.subtle.digest('SHA-1', enc.encode(pwd));
+    var denc = new TextDecoder();
+
+    var encoded = enc.encode(pwd);
+    
+    var prom = crypto.subtle.digest('SHA-1', encoded);
 
     prom.then(function(result) {
-      var denc = new TextDecoder();
-      var hash = denc.decode(result);
+      var hash = hex(result);
       alert(hash);
     }, function(err) {
       alert(err);
@@ -17,3 +20,18 @@ $(function() {
   });
 
 });
+
+//  from https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest#Example
+function hex(buffer) {
+  var hexCodes = [];
+  var view = new DataView(buffer);
+  for (var i = 0; i < view.byteLength; i += 4) {
+    var value = view.getUint32(i)
+    var stringValue = value.toString(16)
+    var padding = '00000000'
+    var paddedValue = (padding + stringValue).slice(-padding.length)
+    hexCodes.push(paddedValue);
+  }
+
+  return hexCodes.join("");
+}
